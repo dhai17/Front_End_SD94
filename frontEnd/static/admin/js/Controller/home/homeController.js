21d14e9301357807ba010b37b8af325cc50c8e7f
LINHNKPH22810\WIN <linhnkph22810@fpt.edu.vn>
2023-10-13 16:58:48 +0700

    creat + list Product

--- frontEnd/static/admin/js/Controller/home/homeController.js
+++ frontEnd/static/admin/js/Controller/home/homeController.js
@@ -14,6 +14,34 @@ app.config(function ($routeProvider) {
             templateUrl: "/templates/admin/discount/create.html",
             controller: "CreateDiscountController",
         })
+        .when("/list-ProductDetails", {
+            templateUrl: "/templates/admin/productDetails/list.html",
+            controller: "ProductDetailsController",
+        })
+        .when("/create-ProductDetails", {
+            templateUrl: "/templates/admin/productDetails/create.html",
+            controller: "EditProductDetailsController",
+        })
+        .when("/edit-ProductDetails", {
+            templateUrl: "/templates/admin/productDetails/edit.html",
+            controller: "EditProductDetailsController",
+        })
+        .when("/list-Product", {
+            templateUrl: "/templates/admin/product/list.html",
+            controller: "ProductController",
+        })
+        .when("/create-Product", {
+            templateUrl: "/templates/admin/product/create.html",
+            controller: "CreateProductController",
+        })
+        .when("/in-store", {
+            templateUrl: "/templates/admin/sales/product_list.html",
+            controller: "ListProductController",
+        })
+        .when("/details-product", {
+            templateUrl: "/templates/admin/sales/details.html",
+            controller: "detailsController",
+        })
         .otherwise({
             redirectTo: "/",
         });
