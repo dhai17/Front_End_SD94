856f114ed7f6561e8b2944554fd2a09438c412bf
leduong2003 <duonglvph23089@fpt.edu.vn>
2023-12-04 15:31:09 +0700

    lduong commit

--- frontEnd/static/auth/login/js/controller/Login.Controller.js
+++ frontEnd/static/auth/login/js/controller/Login.Controller.js
@@ -37,7 +37,7 @@ app.controller('LoginController', function ($scope, $http) {
                 }
 
             }).catch(function (error) {
-                if(error.status === 500){
+                if (error.status === 500) {
                     Swal.fire({
                         icon: "warning",
                         title: "Sai thông tin đăng nhập",
@@ -52,5 +52,24 @@ app.controller('LoginController', function ($scope, $http) {
 
     $scope.returnDangKy = function () {
         window.location.href = "http://127.0.0.1:5501/templates/auth/Register.html#!/";
+    }
+    $scope.quenMatKhau = function () {
+        // Swal.fire({
+        //     icon: "question",
+        //     title: 'Bạn là nhân viên hay khách hàng?',
+        //     showDenyButton: true,
+        //     showCancelButton: true,
+        //     confirmButtonText: 'Nhân viên',
+        //     denyButtonText: 'Khách hàng',
+        //   }).then((result) => {
+        //     if (result.isConfirmed) {
+        //       window.location.href = "http://127.0.0.1:5501/templates/auth/ForgotPassword.html#!/";
+        //     }
+        //      else if (result.isDenied) {
+        //       window.location.href = "http://127.0.0.1:5501/templates/auth/ForgotPasswordKhachHang.html#!/";
+        //     }
+        //   })
+        //   if (result.isConfirmed) {
+            window.location.href = "http://127.0.0.1:5501/templates/auth/ForgotPassword.html#!/";
     }
 }); 
