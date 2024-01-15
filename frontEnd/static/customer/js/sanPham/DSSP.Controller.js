00acdb3ff464f00894650ae1bb0b9c7159dff188
Hai <nghai1702@gmail.com>
2024-01-16 00:15:27 +0700

    haicm

--- frontEnd/static/customer/js/sanPham/DSSP.Controller.js
+++ frontEnd/static/customer/js/sanPham/DSSP.Controller.js
@@ -225,6 +225,15 @@ app.controller('ChiTietSanPhamController', function ($scope, $routeParams, $http
         decodedToken = parseJwt(token);
     }
 
+    $scope.isButtonVisible1 = true;
+    $scope.isButtonVisible2 = true;
+
+    if(decodedToken){
+        $scope.isButtonVisible1 = false;
+    }else{
+        $scope.isButtonVisible2 = false;
+    }
+
     $http
         .get('http://localhost:8080/customer/sanPham/getSanPham/id=' + id_sanPham, { headers })
         .then(function (response) {
