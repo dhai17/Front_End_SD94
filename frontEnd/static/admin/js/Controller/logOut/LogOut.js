f7f68d54a902e5fc43e243fa3cc140a5712b6f8a
leduong2003 <duonglvph23089@fpt.edu.vn>
2024-01-16 14:22:55 +0700

    lduong

--- frontEnd/static/admin/js/Controller/logOut/LogOut.js
+++ frontEnd/static/admin/js/Controller/logOut/LogOut.js
@@ -28,6 +28,18 @@ app.controller('LogoutController', function ($scope, $http) {
         });
     };
 
+    $scope.logoutAdmin = function () {
+        localStorage.removeItem('token');
+        Swal.fire({
+            icon: 'success',
+            title: 'Đang chuyển hướng ra trang đăng nhập',
+            showConfirmButton: false,
+            timer: 2000,
+        }).then(function () {
+            window.location.href = 'http://127.0.0.1:5501/#!/login';
+        });
+    };
+
     $scope.DangKy = function () {
         localStorage.removeItem('token');
         Swal.fire({
